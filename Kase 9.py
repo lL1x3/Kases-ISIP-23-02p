c5bdd4bade4d585f68754d65190983dce116e09a
lL <61538807+lL1x3@users.noreply.github.com>
2024-04-24 16:56:17 +0700

    Create Kase 9.py

---
+++ Kase 9.py
@@ -0,0 +1,89 @@
+# 1
+
+countries1 = ('Russia', 'Argentina', 'Spain', 'Slovakia', 'Canada', 'Slovenia', 'Italy')
+
+last = countries1[6]
+
+print(last)
+
+# 2
+
+primes2 = (2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71)
+
+print(primes2[0:5])
+
+# 3
+
+countries3 = ('Russia', 'Argentina', 'Spain', 'Slovakia', 'Canada', 'Slovenia', 'Italy')
+
+print(countries3[2:8])
+
+# 4
+
+countries4 = ('Russia', 'Argentina', 'Spain', 'Slovakia', 'Canada', 'Slovenia', 'Italy')
+
+print(countries4[0:4])
+
+# 5
+
+countries5 = ('Russia', 'Argentina', 'Spain', 'Slovakia', 'Canada', 'Slovenia', 'Italy')
+
+print(countries5[2:4])
+
+# 6
+
+countries5 = ('Russia', 'Argentina', 'Spain', 'Slovakia', 'Canada', 'Slovenia', 'Italy')
+
+number6 = len(countries5)
+
+print(number6)
+
+# 7
+
+numbers7 = (12.5, 3.1415, 2.718, 9.8, 1.414, 1.1618, 1.324)
+
+minim = min(numbers7)
+maxim = max(numbers7)
+
+total7 = minim + maxim
+
+print(total7)
+
+# 8
+
+countries8 = ('Russia', 'Argentina', 'Spain', 'Slovakia', 'Canada', 'Slovenia', 'Italy')
+
+index = countries8.index('Slovenia')
+
+print(index)
+
+# 9
+
+countries9 = ('Russia', 'Spain', 'Argentina', 'Spain', 'Slovakia', 'Canada', 'Spain', 'Slovenia', 'Italy')
+
+number9 = countries9.count('Spain')
+
+print(number9)
+
+# 10
+
+# 11
+
+city_name = input()
+
+city_year = int(input())
+
+city = (city_name, city_year)
+
+print(city)
+
+# 12
+
+# 13
+
+# 14
+
+numbers10 = (2, 3, 5, 7, -11, 13, 17, 19, 23, 29, 31, -6, 41, 43, 47, 53, 59, 61, -96, 71, 1000, -1)
+
+
+
