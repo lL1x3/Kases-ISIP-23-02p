aa626ccec6614c04f5a9f893540b98c9214897db
lL <61538807+lL1x3@users.noreply.github.com>
2024-06-03 21:05:28 +0700

    Add files via upload

---
+++ kase2.py
@@ -0,0 +1,83 @@
+# пароль
+
+# pass1 = input()
+# pass11 = input(
+# if pass11 == pass1:
+    # print("Пароли совпадают")
+# else:
+    # print("Пароли не совпадают")
+
+# Четное или нечетное
+
+# num2 = int(input())
+# if num2 % 2 == 0:
+#     print("Число четное")
+# else:
+#     print("Число нечетное")
+
+# Соотношение
+
+# num3 = int(input())
+# first = num3 // 1000
+# second = num3 // 100 - (num3 // 1000 * 10)
+# third = (num3 % 100 - num3 % 10) / 10
+# last = num3 % 10
+# if first + last == second - third:
+#     print('ДА')
+# else:
+#     print('НЕТ')
+
+# Роскомнадзор
+
+# age4 = int(input())
+# if age4 >= 18:
+#     print('Доступ разрешен')
+# else:
+#     print('Доступ запрещен')
+
+# Арифметическая прогрессия
+
+# a5 = int(input())
+# b5 = int(input())
+# c5 = int(input())
+# if b5 - a5 == c5 - b5:
+#     print('YES')
+# else:
+#     print('NO')
+
+# Наименьшее из двух чисел
+
+# a6 = int(input())
+# b6 = int(input())
+# if a6 > b6:
+#     print(b6,'Меньше',a6)
+# elif a6==b6:
+#     print("Числа равны")
+# else:
+#     print(a6, 'Меньше', b6)
+
+# Наименьшее из четырех чисел
+
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# d = int(input())
+# if a > b:
+#     a = b
+# if c > d:
+#     c = d
+# if a > c:
+#     a = c
+# print(a)
+
+# Возрастная группа
+
+# age8 = int(input())
+# if age8 >=60:
+#     print("Старость")
+# elif age8 >=25:
+#     print("Зрелость")
+# elif age8 >=14:
+#     print("Молодость")
+# else:
+#     print("Детство")
