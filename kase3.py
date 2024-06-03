d16f43b345a18c2b3fa4c41d123d22fe8edec3d4
lL <61538807+lL1x3@users.noreply.github.com>
2024-06-03 21:11:08 +0700

    Add files via upload

---
+++ kase3.py
@@ -0,0 +1,61 @@
+# Python is awesome!
+
+# for i in range(10):
+#     print('Python is awesome!')
+
+# Повторяй за мной 1
+
+# A = input()
+# B = int(input())
+# for i in range(B):
+#     print(A)
+
+# Последовательность символов
+
+# for i in range(6):
+#     print('AAA')
+# for i in range(5):
+#     print('BBBB')
+# print('E')
+# for i in range(9):
+#     print('TTTTT')
+# print('G')
+
+# Звездный прямоугольник
+
+# n = int(input())
+# for i in range(n):
+#     print('*' * 19)
+
+# Повторяй за мной 2
+
+# a = input()
+# for b in range(10):
+#     print(b, a)
+
+# Квадрат числа
+
+# n = int(input())
+# for i in range(n + 1):
+#     print('Квадрат числа',i,'равен',i ** 25)
+
+# Звездный треугольник
+
+# n = int(input())
+#     print('*' * (n - i))
+
+# Remove outliners
+
+# print('Введите кол-во строк')
+# n = int(input())
+# mainlist = list()
+# secondlist = list()
+# print('Введите', n, 'чисел')
+# for i in range(n):
+#     mainlist.append(int(input()))
+# print(mainlist)
+# min_num = min(mainlist)
+# max_num = max(mainlist)
+# mainlist.remove(min_num)
+# mainlist.remove(max_num)
+# print('Ваши числа без минимального и максимального', *mainlist, sep='\n')
