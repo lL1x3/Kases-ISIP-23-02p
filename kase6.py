a52328deb6b4c861cf75d8424d3cfa13ac18489c
lL <61538807+lL1x3@users.noreply.github.com>
2024-06-04 20:17:03 +0700

    Add files via upload

---
+++ kase6.py
@@ -0,0 +1,49 @@
+
+# Все сразу 1
+
+# numbers = [2, 6, 3, 14, 10, 4, 11, 16, 12, 5, 4, 16, 1, 0, 8, 16, 10, 10, 8, 5, 1, 11, 10, 10, 12, 0, 0, 6, 14, 8, 2,
+#            12, 14, 5, 6, 12, 1, 2, 10, 14, 9, 1, 15, 1, 2, 14, 16, 6, 7, 5]
+# print(len(numbers))
+# print(numbers[-1])
+# print(numbers[::-1])
+# if 5 in numbers and 17 in numbers:
+#     print('YES')
+# else:
+#     print('NO')
+# del numbers[0]
+# del numbers[-1]
+# print(numbers)
+
+# Список строк
+
+# n = int(input())
+# brabra = []
+# for _ in range(n):
+#     brabra.append(input())
+# print(brabra)
+
+# Алфавит
+
+# абв = ['.', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+# for i in range(1,27):
+#     print(абв[i]*i)
+
+# Список кубов
+
+# list = []
+# n = int(input())
+# for _ in range(n):
+#     num = int(input())
+#     cube = num ** 3
+#     list.append(cube)
+# print(list)
+
+
+# Суммы двух
+
+# n, a, c = int(input()), int(input()), []
+# for i in range(n - 1):
+#     b = int(input())
+#     c.append(a + b)
+#     a = b
+# print(c)
