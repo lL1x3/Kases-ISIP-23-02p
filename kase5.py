cd6d602eaaabf7489e01c8efb07578363efb2947
lL <61538807+lL1x3@users.noreply.github.com>
2024-06-04 17:49:24 +0700

    Add files via upload

---
+++ kase5.py
@@ -0,0 +1,35 @@
+# В столбик 2
+
+# a = '123456789'
+# b =  len(a)
+# for i in range(b+1):
+#     print(a[-i])
+
+# Фио
+
+# name2 = input()
+# surname2 = input()
+# patronymic2  = input()
+# print(name2[0],surname2[0],patronymic2[0])
+
+# Цифра 2
+
+# bb = input()
+# beb = bb.isalpha()
+# if beb == True:
+#     print('Цифр нет')
+# else:
+#     print('Цифра')
+
+# Сколько раз?
+
+# num4 = input()
+# star = 0                 # Создаем переменную для звездочки
+# plus = 0                 # Создаем переменную для плюса
+# for i in num4:
+#     if i == '*':         # Если встретилась звездочка
+#         star += 1        # То прибавляем +1
+#     elif i == '+':       # Иначе проверяем это плюс или нет
+#         plus += 1
+# print('Символ + встречается', plus, 'раз')
+# print('Символ * встречается', star, 'раз')
