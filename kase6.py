12417fc4d2332c554f2a86d8c13f85f07369f666
lL <61538807+lL1x3@users.noreply.github.com>
2024-06-07 11:16:11 +0700

    Add files via upload

--- kase6.py
+++ kase6.py
@@ -1,49 +1,66 @@
+# Звездный прямоугольник
 
-# Все сразу 1
+# def draw_box():
+#     print("*"*10)
+#     for  _ in range(12):
+#         print("*", ' ' * 6, "*")
+#     print("*" * 10)
+# draw_box()
 
-# numbers = [2, 6, 3, 14, 10, 4, 11, 16, 12, 5, 4, 16, 1, 0, 8, 16, 10, 10, 8, 5, 1, 11, 10, 10, 12, 0, 0, 6, 14, 8, 2,
-#            12, 14, 5, 6, 12, 1, 2, 10, 14, 9, 1, 15, 1, 2, 14, 16, 6, 7, 5]
-# print(len(numbers))
-# print(numbers[-1])
-# print(numbers[::-1])
-# if 5 in numbers and 17 in numbers:
-#     print('YES')
-# else:
-#     print('NO')
-# del numbers[0]
-# del numbers[-1]
-# print(numbers)
+# Звездный треугольник 1
 
-# Список строк
+# def draw_triangle():
+#     for i in range(10):
+#         print("*"*i)
+# draw_triangle()
 
-# n = int(input())
-# brabra = []
-# for _ in range(n):
-#     brabra.append(input())
-# print(brabra)
+# Звездный треугольник
 
-# Алфавит
+# Определение функции draw_triangle()
 
-# абв = ['.', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-# for i in range(1,27):
-#     print(абв[i]*i)
+# def draw_triangle(base,fill):
+    # for i in range(base // 2):
+    #     print(fill * (i + 1))
+    # for i in range(base // 2, -1, -1):
+    #     print(fill * (i + 1))
+# draw_triangle(5,'*')
 
-# Список кубов
+# ФИО
 
-# list = []
-# n = int(input())
-# for _ in range(n):
-#     num = int(input())
-#     cube = num ** 3
-#     list.append(cube)
-# print(list)
+# def print_fio(name,surname,patronymic):
+#     print(name[0],surname[0],patronymic[0])
+# print_fio('Пушкин','Александр','Сергеевич')
 
+# Сумма цифр
 
-# Суммы двух
+# def print_digit_sum():
+#     num  = int(input())
+#     n =0
+#     while num > 0:
+#         n += num % 10
+#         num //= 10
+#     print(n)
+# print_digit_sum()
 
-# n, a, c = int(input()), int(input()), []
-# for i in range(n - 1):
-#     b = int(input())
-#     c.append(a + b)
-#     a = b
-# print(c)
+# Конвертер километров
+
+# def convert_to_miles(km):
+#     miles = km  *  0.6214
+#     print(miles)
+# convert_to_miles(1)
+
+# Количество дней
+
+# def get_days(month):
+#     if month in [1, 3, 5, 7, 8, 10, 12]:
+#         days = 31
+#     elif month in [4, 6, 9, 11]:
+#         days = 30
+#     else:
+#         days = 28
+#     print(days)
+# get_days(1)
+
+# Делители 1
+
+# Делители 2
